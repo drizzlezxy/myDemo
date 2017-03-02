8129686238753912871cdfa81780d5dbcb96c91b
zhangxiaoyu <drizzlezxy@126.com>
2017-03-02 15:42:17 +0800

    React API this.setState(()=>{})

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "env": {
+        "browser": true
+    "jQuery": true,
+    "node": true
+    },
+    "extends": "airbnb",
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
